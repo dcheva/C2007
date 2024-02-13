ed511ae57647503e9b28b843fdfb533e55160d45
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-02-13 10:09:44 +0200

    FUCK!!! use dee211.bat

--- dee211.bat
+++ dee211.bat
@@ -1,10 +1,10 @@
 @echo del /F /Q "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.bin" "C:\Documents and Settings\Dmytro\AppData\Local\NVIDIA\DXCache\*.toc"
 pushd C:\Projects\C2077
 git add -A
-git commit -am "Autogit dee21 %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
+git commit -am "Autogit dee211 %date% at %time:~0,2%:%time:~3,2%:%time:~6,2%"
 git pull
 git push
-git checkout dee21
+git checkout dee211
 pushd "C:\Games\Cyberpunk 2077\bin\x64"
 cd "C:\Games\Cyberpunk 2077\bin\x64"
 start Cyberpunk2077.exe
