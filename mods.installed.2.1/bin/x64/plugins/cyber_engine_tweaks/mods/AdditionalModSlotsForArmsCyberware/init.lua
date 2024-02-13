1245680fcc966b1f4cc8e504dc5bccab71e10aa1
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-02-13 08:50:14 +0200

    Ready to update

---
+++ mods.installed.2.1/bin/x64/plugins/cyber_engine_tweaks/mods/AdditionalModSlotsForArmsCyberware/init.lua
@@ -0,0 +1,13 @@
+registerForEvent("onInit", function() 
+		
+    TweakDB:SetFlat("Items.StrongArmsBlueprint_inline0.childElements", {"Items.StrongArmsBlueprint_inline1", "Items.StrongArmsBlueprint_inline3", "Items.StrongArmsBlueprint_inline4", "Items.MantisBladesBlueprint_inline2", "Items.NanoWiresBlueprint_inline2", "Items.ProjectileLauncherBlueprint_inline2", "Items.GenericMod1Element"})
+
+    TweakDB:SetFlat("Items.ProjectileLauncherBlueprint_inline0.childElements", {"Items.ProjectileLauncherBlueprint_inline1", "Items.ProjectileLauncherBlueprint_inline2", "Items.ProjectileLauncherBlueprint_inline4", "Items.MantisBladesBlueprint_inline2", "Items.NanoWiresBlueprint_inline2", "Items.StrongArmsBlueprint_inline1", "Items.GenericMod1Element"})
+
+    TweakDB:SetFlat("Items.MantisBladesBlueprint_inline0.childElements", {"Items.MantisBladesBlueprint_inline1", "Items.MantisBladesBlueprint_inline2", "Items.MantisBladesBlueprint_inline4", "Items.ProjectileLauncherBlueprint_inline2", "Items.NanoWiresBlueprint_inline2", "Items.StrongArmsBlueprint_inline1", "Items.GenericMod1Element"})
+
+    TweakDB:SetFlat("Items.NanoWiresBlueprint_inline0.childElements", {"Items.NanoWiresBlueprint_inline1", "Items.NanoWiresBlueprint_inline2", "Items.NanoWiresBlueprint_inline4", "Items.ProjectileLauncherBlueprint_inline2", "Items.MantisBladesBlueprint_inline2", "Items.StrongArmsBlueprint_inline1", "Items.GenericMod1Element"})
+
+end)
+		
+
